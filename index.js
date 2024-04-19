96e86228aa171f019ca074873e1ea756a3bb2279
unknown <umanitore@gmail.com>
2024-04-19 13:07:36 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const FsManager = require("./lib/general/FsManager.js");
+const FarmManager = require("./lib/main/FarmManager.js");
+const InputManager = require("./lib/general/InputManager.js");
+const settings = require("./settings.js");
+
+const chalk = require("chalk");
+
+const path = require("path");
+const cwd = process.cwd();
+
+!async function main() {
+
+    while(true) {
+        const res = await startFarmManager();
+    }
+
+    async function startFarmManager() {
+        const farmManager = new FarmManager(settings.farmPath);
+
+        const topic = await InputManager.select(({
+            choices: ["Tg", "Data", "Others", "..."],
+            message: "Pick a topic: ",
+        }));
+
+        let actionChoices;
+        switch(topic.trim()) {
+            case("Tg"): {
+                actionChoices = ["LaunchAccounts", "LaunchAccountsTemp", "Import", "Export", "UpdateTg", "..."];
+                break;
+            } 
+            case("Data"): {
+                actionChoices = ["EnumImages", "..."];
+                break;
+            } 
+            case("Others"): {
+                actionChoices = ["CorrectMnemoTG",  "CreateFarm", "..."];
+                break;
+            }
+            case("..."): {
+                process.exit(0);
+                break;
+            }
+            default: {
+                console.log(chalk.red("No such action :'("));
+                return false;
+            }
+        }
+
+        const action = await InputManager.select(({
+            choices: actionChoices,
+            message: "Pick an action: ",
+        }));
+
+        const actionRes = await farmManager.act({action, });
+
+
+        return true;
+    }
+}()
---
+++ settings.js
@@ -0,0 +1,6 @@
+const farmPath = String.raw``;
+
+module.exports = {
+    farmPath,
+
+}
